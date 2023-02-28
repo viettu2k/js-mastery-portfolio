c369b82c51e9bebcb93e70cf018ce51cd49b7868
viettu2k <doanviettu2k@gmail.com>
2023-02-28 22:40:11 +0700

    adding folder struceture ui

---
+++ frontend-react/src/container/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
---
+++ frontend-react/src/container/Work/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Works = () => {
+  return <div>Works</div>;
+};
+
+export default Works;
