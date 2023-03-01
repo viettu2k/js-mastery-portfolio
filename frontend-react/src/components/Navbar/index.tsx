98bbe762b58c907dfd2852ae834d64e43d73fc6c
viettu2k <doanviettu2k@gmail.com>
2023-03-01 22:46:43 +0700

    implementing navbar

--- frontend-react/src/components/Navbar/index.tsx
+++ frontend-react/src/components/Navbar/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 import './styles.scss';
+import { images } from '../../constants';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'skills', 'contact', 'work', 'skills'].map(
+          (item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}> {item}</a>
+            </li>
+          )
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
