13edd9b0736fcef0733193cc0dca44a06b596cd2
viettu2k <doanviettu2k@gmail.com>
2023-03-04 22:57:44 +0700

    implementing header

--- frontend-react/src/container/Header/index.tsx
+++ frontend-react/src/container/Header/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './styles.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='bage-cmp app__flex'>
+            <span>┴</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>VietTu</h1>
+            </div>
+          </div>
+
+          <div className='tag-cmp app__flex'></div>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
