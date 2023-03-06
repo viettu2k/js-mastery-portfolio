f295445fc07e166b2aa294026dc5e65718059560
viettu2k <doanviettu2k@gmail.com>
2023-03-06 23:07:31 +0700

    adding about container

--- frontend-react/src/container/About/index.tsx
+++ frontend-react/src/container/About/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './styles.scss';
+import { client, urlFor } from '../../client';
 
 const About = () => {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data: any) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className='head-text'>
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about: any, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl) as any} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default About;
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg'
+);
