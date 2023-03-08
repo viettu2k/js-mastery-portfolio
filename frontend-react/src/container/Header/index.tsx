d75d2bd002f4113ed071bab1534692115b3c4776
viettu2k <doanviettu2k@gmail.com>
2023-03-08 23:19:24 +0700

    update

--- frontend-react/src/container/Header/index.tsx
+++ frontend-react/src/container/Header/index.tsx
@@ -34,7 +34,6 @@ const Header = () => (
 
         <div className='tag-cmp app__flex'>
           <p className='p-text'>Web Developer</p>
-          <p className='p-text'>Freelancer</p>
         </div>
       </div>
     </motion.div>
